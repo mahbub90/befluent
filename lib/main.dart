3c1a5abde6e4771d371c8b6ba7a2a265928e412a
mahbub-hussain@hotmail.com <90alPHa1>
2020-04-08 23:46:22 +0200

    Front screen fully done. - Title and subtitle added, and everything placed correctly on the screen. - Background image put in a Stack widget so it's behind everything

--- lib/main.dart
+++ lib/main.dart
@@ -4,60 +4,124 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  static final primaryColor = const Color(0xFFFAEFE3);
-  static final buttonColor = const Color(0xFFEC615B);
-  static final secondaryButtonColor = const Color(0xFF8B70D4);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Gilroy'),
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = const Color(0xFFFAEFE3);
+    final textColorOrange = const Color(0xFFFF5454);
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: primaryColor,
-        body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            Image(image: AssetImage("assets/login_illustration.png")),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: Container(
+            width: screenWidth,
+            alignment: Alignment.center,
+            /*decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/login_illustration.png"),
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              ),
+            ),*/
+            child: Stack(
+              fit: StackFit.expand,
               children: <Widget>[
-                ButtonTheme(
-                  minWidth: 280,
-                  child: RaisedButton(
-                    color: buttonColor,
-                    child: Text(
-                      "LOG IN",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal,
-                      fontFamily: 'North'),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 18),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(28)),
-                    onPressed: () {},
+                Positioned(
+                  top: 50,
+                  width: screenWidth,
+                  child: Image.asset(
+                    "assets/login_illustration.png",
+                    fit: BoxFit.fitWidth,
                   ),
                 ),
-                ButtonTheme(
-                  minWidth: 150,
-                  child: RaisedButton(
-                    color: secondaryButtonColor,
-                    child: Text(
-                      "REGISTER",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 50, bottom: 10),
+                      child: RichText(
+                        text: TextSpan(
+                            text: "Dryve",
+                            style: TextStyle(
+                                color: textColorOrange,
+                                fontSize: 30,
+                                fontFamily: "Gilroy",
+                                fontWeight: FontWeight.w700),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: 'Academy',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 30,
+                                      fontFamily: "Gilroy"))
+                            ]),
+                      ),
                     ),
-                    padding: EdgeInsets.symmetric(vertical: 8),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24)),
-                    onPressed: () {},
-                  ),
+                    Text(
+                      "Start your journey today",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  width: screenWidth,
+                  bottom: screenHeight / 10,
+                  child: Column(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: ButtonTheme(
+                        minWidth: 280,
+                        child: RaisedButton(
+                          color: Color(0xFFEC615B),
+                          child: Text(
+                            "LOG IN",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 14),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(28)),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                    ButtonTheme(
+                      minWidth: 150,
+                      child: RaisedButton(
+                        color: Color(0xFF8B70D4),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24)),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ]),
                 ),
               ],
-            )
-          ],
-        )),
+            )),
       ),
     );
   }
